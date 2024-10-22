8332e4deda134fdc212210ca57ed38408652f37d
vulcanshen <vulcan.shen@tpisoftware.com>
2024-10-22 16:16:42 +0800

    native build

---
+++ native-image/src/main/java/vulcan/springboot/native_image/NativeImageApplication.java
@@ -0,0 +1,13 @@
+package vulcan.springboot.native_image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeImageApplication.class, args);
+	}
+
+}
---
+++ native-image/src/test/java/vulcan/springboot/native_image/NativeImageApplicationTests.java
@@ -0,0 +1,13 @@
+package vulcan.springboot.native_image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
